79d524fba94bd249e39a4f91a460e8b53d99f8f0
NakulPusadkar <nakulpusadkar2306@gmail.com>
2025-06-18 16:05:40 +0530

    18 June 2025

---
+++ src/test/java/org/testingacademy/ex_05_Selenium_Waits/TestSelenium22_Waits.java
@@ -0,0 +1,29 @@
+package org.testingacademy.ex_05_Selenium_Waits;
+
+import io.qameta.allure.Description;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+public class TestSelenium22_Waits {
+
+    @Description("Verify Ebay Print the prices (iMac)")
+    @Test
+    public void test_verify_print_imac_price() {
+
+        EdgeDriver driver = new EdgeDriver();
+        driver.get("https://app.vwo.com");
+
+     //   driver.manage().timeouts().pageLoadTimeout(30, TimeUnit.SECONDS);
+
+        try {
+            Thread.sleep(20000); // JVM to halt
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        driver.quit();
+    }
+
+}
---
+++ src/test/java/org/testingacademy/ex_08_SVG_Elements/Lab30_SVG.java
@@ -0,0 +1,65 @@
+package org.testingacademy.ex_08_SVG_Elements;
+
+import io.qameta.allure.Description;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import java.util.List;
+
+import static org.testingacademy.WaitHelper.WaitHelper.checkVisibility;
+import static org.testingacademy.WaitHelper.WaitHelper.waitJVM;
+
+public class Lab30_SVG {
+
+    EdgeDriver driver;
+
+
+    @BeforeTest
+    public void openBrowser(){
+        EdgeOptions edgeOptions = new EdgeOptions();
+        edgeOptions.addArguments("--guest");
+        driver = new EdgeDriver(edgeOptions);
+        driver.manage().window().maximize();
+
+    }
+
+
+    @Description("Verify that the on search with svg icon results are visible.")
+    @Test
+    public void test_Flipkart_Search_SVG_Icon(){
+
+        String URL = "https://www.flipkart.com/search";
+        driver.get(URL);
+
+        // Step 1 - Enter the "macnmini" in the inputbox.
+        driver.findElement(By.name("q")).sendKeys("macmini");
+
+        // Step 2 - Click on the svg element.
+        List<WebElement> svgElements = driver.findElements(By.xpath("//*[local-name()='svg']"));
+        svgElements.get(0).click();
+
+        checkVisibility(driver,By.xpath("//div[contains(@data-id,'CPU')]/div/a[2]"));
+
+
+        List<WebElement> titlesResults = driver.findElements(By.xpath("//div[contains(@data-id,'CPU')]/div/a[2]"));
+        for (WebElement title :  titlesResults){
+            System.out.println(title.getText());
+        }
+
+    }
+
+
+    @AfterTest
+    public void closeBrowser(){
+        waitJVM(10000);
+        driver.quit();
+    }
+
+
+
+}
